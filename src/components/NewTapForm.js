5de2f271fec26d73822a140f338c7f23e706b023
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 09:18:34 -0700

    add newtapform component

---
+++ src/components/NewTapForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+
+
+function NewTapForm(props){
+
+  return (
+    
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+
+export default NewTapForm;
