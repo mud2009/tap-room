81c79e6f3c2c853b5faaff03aa317bf81d12c8d0
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 10:31:40 -0700

    map through taplist and tap component to dom

--- src/components/TapList.js
+++ src/components/TapList.js
@@ -1,11 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Tap from "./Tap";
 
-function TapList(){
+function TapList(props){
   return(
     <React.Fragment>
-
+      {props.tapList.map(tap => 
+        <Tap 
+          name={tap.name}
+          brand={tap.brand}
+          price={tap.price}
+          alcoholContent={tap.alcoholContent}
+          volume={tap.volume}
+          id={tap.id}
+          key={tap.id}/>
+        )}
     </React.Fragment>
   );
+}
+
+TapList.propTypes = {
+  tapList: PropTypes.array,
+  onTapSelection: PropTypes.func
 }
 
 export default TapList;
