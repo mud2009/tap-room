f4a6ecdede5824540f147001fbb997f7eab02fee
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 14:13:24 -0700

    add delete button to tap detail

--- src/components/TapDetail.js
+++ src/components/TapDetail.js
@@ -2,20 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TapDetail(props){
-  const {tap} = props;
+  const {tap, onDeleteTap} = props;
   return(
     <React.Fragment>
-      <h3>{tap.name}'s Details</h3>
-      <p>Brand: {tap.brand}</p>
-      <p>Price: ${tap.price}</p>
-      <p>ABV: {tap.alcoholContent}%</p>
-      <h3>Pints left: {tap.volume}</h3>
+      <div>
+        <h3>{tap.name}'s Details</h3>
+        <p>Brand: {tap.brand}</p>
+        <p>Price: ${tap.price}</p>
+        <p>ABV: {tap.alcoholContent}%</p>
+        <h3>Pints left: {tap.volume}</h3>
+        <button onClick={() => onDeleteTap(tap.id)}>Remove Tap</button>
+      </div>
+      <br/>
     </React.Fragment>
   );
 }
 
 TapDetail.propTypes = {
   tap: PropTypes.object,
+  onDeleteTap: PropTypes.func
 }
 
 export default TapDetail;
