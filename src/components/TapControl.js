9113c26e9a917a60da8c2bbee080ddb7b1d6d998
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 09:00:23 -0700

    add header and tapcontrol components

---
+++ src/components/TapControl.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class TapControl extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      formVisible: false,
+      mainTapList: [],
+      selectedTap: null
+    }
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+  
+      </React.Fragment>
+    )  
+  }
+}
+
+export default TapControl;
