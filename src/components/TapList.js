2ab5c8a091eedbaee0d4d41447ec585a70a7b09f
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 11:24:08 -0700

    add conditional rendering, so if the taplist is empty, the dom renders an add tap prompt

--- src/components/TapList.js
+++ src/components/TapList.js
@@ -3,21 +3,29 @@ import PropTypes from "prop-types";
 import Tap from "./Tap";
 
 function TapList(props){
-  return(
-    <React.Fragment>
-      {props.tapList.map(tap => 
-        <Tap 
-          whenTapClicked={props.onTapSelection}
-          name={tap.name}
-          brand={tap.brand}
-          price={tap.price}
-          alcoholContent={tap.alcoholContent}
-          volume={tap.volume}
-          id={tap.id}
-          key={tap.id}/>
-        )}
-    </React.Fragment>
-  );
+  if(props.tapList.length === 0){
+    return (
+      <React.Fragment>
+        <h4>No taps added yet!</h4>
+      </React.Fragment>
+    );
+  } else {
+    return(
+      <React.Fragment>
+        {props.tapList.map(tap => 
+          <Tap 
+            whenTapClicked={props.onTapSelection}
+            name={tap.name}
+            brand={tap.brand}
+            price={tap.price}
+            alcoholContent={tap.alcoholContent}
+            volume={tap.volume}
+            id={tap.id}
+            key={tap.id}/>
+          )}
+      </React.Fragment>
+    );
+  }
 }
 
 TapList.propTypes = {
