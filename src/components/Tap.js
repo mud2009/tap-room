85e8628a9ae593de596b0f06d7e63a58e8da869a
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 10:30:45 -0700

    add html to display tap properties on tap component

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,11 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Tap(){
+function Tap(props){
   return(
     <React.Fragment>
-
+      <div>
+        <h4>{props.name}</h4>
+        <p>Brand: {props.brand}</p>
+        <p>Price: ${props.price}</p>
+        <p>ABV: {props.alcoholContent}%</p>
+        <p>Pints left in keg: {props.volume}</p>
+      </div>
     </React.Fragment>
   )
+}
+
+Tap.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  alcoholContent: PropTypes.number,
+  volume: PropTypes.number
 }
 
 export default Tap;
