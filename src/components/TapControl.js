1968786a06f9ddb43024dc47caf732fd660a1686
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 09:37:08 -0700

    add base branch rendering for tap control

--- src/components/TapControl.js
+++ src/components/TapControl.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NewTapForm from "./NewTapForm";
+import TapDetail from "./TapDetail";
+import TapList from "./TapList";
 
 class TapControl extends React.Component{
 
@@ -12,9 +15,24 @@ class TapControl extends React.Component{
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.selectedTap !== null){
+      currentlyVisibleState = <TapDetail tap = {this.state.selectedTap} />
+      buttonText = "Return to tap list";
+    } else if (this.state.formVisible){
+      currentlyVisibleState = <NewTapForm />
+      buttonText= "Return to tap list";
+    } else {
+      currentlyVisibleState = <TapList tapList={this.state.mainTapList}/>
+      buttonText= "Add tap";
+    }
+
     return(
       <React.Fragment>
-  
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     )  
   }
