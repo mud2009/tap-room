1813f07b874315a2eb0224cbed933f2964814976
Will Greenberg <greenberg.will@gmail.com>
2022-04-22 11:47:36 -0700

    add proptype for tap prop, add tap properties to details

--- src/components/TapDetail.js
+++ src/components/TapDetail.js
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function TapDetail(){
+function TapDetail(props){
+  const {tap} = props;
   return(
     <React.Fragment>
-
+      <h3>{tap.name}'s Details</h3>
+      <p>Brand: {tap.brand}</p>
+      <p>Price: ${tap.price}</p>
+      <p>ABV: {tap.alcoholContent}%</p>
+      <h3>Pints left: {tap.volume}</h3>
     </React.Fragment>
   );
+}
+
+TapDetail.propTypes = {
+  tap: PropTypes.object,
 }
 
 export default TapDetail;
